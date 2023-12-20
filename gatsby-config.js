7c725dd69c8c80ce1032b6b8caabbd0538db0a52
Joel Bränninger <joel.branninger@live.se>
2023-12-20 09:54:25 +0100

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,16 @@
+/**
+ * @type {import('gatsby').GatsbyConfig}
+ */
+module.exports = {
+  siteMetadata: {
+    title: `Portfolio`,
+    siteUrl: `https://www.yourdomain.tld`
+  },
+  plugins: [{
+    resolve: 'gatsby-source-contentful',
+    options: {
+      "accessToken": "mMiEngGVUj8LqaylQS3tUsjeMy0jFUxEjTK_dWllmB0",
+      "spaceId": "q8ftcuzlu439"
+    }
+  }, "gatsby-plugin-image", "gatsby-plugin-sharp", "gatsby-transformer-sharp", "gatsby-plugin-postcss"]
+};
